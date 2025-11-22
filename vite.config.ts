89861d0fdf644101e95b16120ec850278129343f
charmi <deimylolgamer0@gmail.com>
2025-11-22 10:38:57 -0300

    implementacion vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',    // Para testear React
+    globals: true,           // describe, test, expect
+    setupFiles: './src/test/setup.ts',
+    css: true
+  }
 })
